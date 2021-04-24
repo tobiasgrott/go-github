385db682936f3cccbc80114c2617ff140766b95f
Tobias Grott <tobiasgrott@gmail.com>
2021-04-24 10:47:00 +0200

    added go test

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func Sum(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("Hi, DEV World! 👍")
+	fmt.Printf("The sum of %d and %d is %d", 5, 5, Sum(5, 5))
 }
